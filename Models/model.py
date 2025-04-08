336f9a010db2cb7a046d251ae45f2cacfce4cbb9
Emma Paris <Bewantza33@gmail.com>
2025-04-08 16:42:32 +0700

    implement new code

--- Models/model.py
+++ Models/model.py
@@ -4,36 +4,47 @@ from enum import Enum
 
 class SentimentItem(BaseModel):
     """ Represents sentiment anlysis of social comment.
-  - explanation: explained in detailed why you choose this sentiment.
-  - output:the sentiment value of phase for TRUE company. if it's sarcasm, considerate it as negative.
+    - explanation: explained in detailed why you choose this sentiment.
+    - output:the sentiment value of phase for TRUE company. if it's sarcasm, considerate it as negative.
+    **MUST ANSWER EVERY PHASE!!**
     """
     explanation: str
     output: Literal["Positive", "Negative", "Neutral"]
 
-class TRUEItem(BaseModel):
-    """Represents if phase related to TRUE company or not.
-    - explanation: explained in detailed why this phase related to TRUE company.
-    - output is this phase related to TRUE company, only if it mention in phase that it's about TRUE. NOTE: synonym of TRUE is 'ค่ายแดง','ค่ายตัว t'.
+class TRUEDTACItem(BaseModel):
+    """Represents if phase related to TRUE or DTAC company or not.
+    - explanation: explained in detailed why this phase related to TRUE or DTAC company.
+    - output is this phase related to TRUE or DTAC company, only if it mention in phase that it's about TRUE or DTAC.
+    NOTE: synonym of TRUE is 'ค่ายแดง','ค่ายตัว t'.
+          synonym of DTAC is 'Dtac', 'DTAC', 'ดีแทค', 'ค่ายฟ้า', 'detac', 'dtac', '1678'
+    NOTE: "Nan" means not related to TRUE or DTAC.
+    **MUST ANSWER EVERY PHASE!!**
     """
     explanation: str
-    output: Literal[True, False]
+    output: Literal["True", "Dtac", "Nan"]
 
-class ADS(BaseModel):
-    """Represents commercial ads and commercial phase detection.
-    - explanation: explained in detailed why you this this phase is commercial.
-    - output is this phase is ads.
-    """
-    explanation: str
-    ADS: Literal[True, False]
+# class ADS(BaseModel):
+#     """Represents commercial ads and commercial phase detection.
+#     - explanation: explained in detailed why you this this phase is commercial.
+#     - output is this phase is ads.
+#     **MUST ANSWER EVERY PHASE!!**
+#     """
+#     explanation: str
+#     ADS: Literal[True, False]
 
 class SentimentTrue(BaseModel):
     Sentiment: list[SentimentItem]
-    IS_TRUE: list[TRUEItem]
-    ADS: list[ADS]
+    IS_TRUE: list[TRUEDTACItem]
+    # ADS: list[ADS]
+
+class ClassifyItem(BaseModel):
+    """Represents classification of TRUE or DTAC company.
+    - explanation: explained in detailed why you choose classify for this phases.
+    **MUST ANSWER EVERY PHASE!!**
+    """
+    explanation: str
 
 class OutputOptions(str, Enum):
-    Nan = "Nan"
-    True_ = "True"
     True_5G = "True 5G"
     True_Visions = "True Visions"
     True_Visions_NOW = "True Visions NOW"
@@ -42,19 +53,15 @@ class OutputOptions(str, Enum):
     True_ID = "True ID"
     True_iService = "True iService"
     True_Corp = "True Corp"
+    True_ = "True"
+    Dtac = "Dtac"
+    Nan = "Nan"
 
 class Item(BaseModel):
     """จะตอบคำถาม user ต้องแสดงข้อมูลอะไรบ้างจากข้อมูลข้างล่าง
     คำอธิบาย:
 
-    "์Nan" - ในข้อความไม่ได้เกี่ยวอะไรกับ product TRUE หรือ ผลิตภัณฑ์ หรือ ไม่มีการพูดถึง ทรู เช่น สนใจค่ะ
-
-    "True" - หมายถึงการพูดถึงทรูในลักษณะกว้าง ๆ โดยไม่ได้ระบุผลิตภัณฑ์หรือบริการเฉพาะเจาะจง
-    เช่น การกล่าวถึงบริษัททรู, แบรนด์ทรู, หรือบริการทรูโดยรวม หากในข้อความมีการพูดถึงคำว่า
-    'ทรู' หรือ 'True' โดยไม่มีคำอื่นที่เกี่ยวข้องกับผลิตภัณฑ์หรือบริการเฉพาะ
-    - **!!หากไม่สามารถ จำแนก product ทรูได้ชัดเจน ให้ตอบ "TRUE"**
-
-    "True 5G" - ชื่อเดิม True 5G = True Move H
+    "True 5G" - ชื่อเดิม True 5G = True Move H, dtac 5G = dtac
     อินเตอร์เน็ตมือถือ, ให้บริการเชื่อมต่อเครือข่ายที่แข็งแกร่งและรวดเร็วครอบคลุมทั่วไทย
     ในฐานะผู้นำบริการโทรคมนาคมเพียงรายเดียวที่มีคลื่นความถี่ครบทุก 8 ย่าน ทั้งเครือข่าย 5G และ 4G
     ด้วยจำนวนเสาสัญญาณที่มีมากกว่าหลายหมื่นแห่งทั่วประเทศ พร้อมให้บริการเครือข่ายครอบคลุม 99% ของประชากรแล้ว
@@ -79,23 +86,41 @@ class Item(BaseModel):
 
     "True iService" - แอปพลิเคชัน เว็บไซต์ ช่องทางอำนวยความสะดวกสำหรับลูกค้า ในการเข้าถึงบริการและผลิตภัณฑ์ต่างๆ ในกลุ่มทรู
     ไม่ว่าจะเป็นการตรวจสอบยอดการใช้งาน ประวัติใบแจ้งค่าบริการ ชำระค่าบริการ เติมเงิน สมัครแพ็กเกจ เสริม ตรวจสอบข้อมูลส่วนตัวได้ด้วยตัวคุณเองในรูปแบบออนไลน์
+    ทรู ไอเซอร์วิส, ดีแทค แอปฯ
 
     "True Corp" - เดิม: บริษัท ทรู คอร์ปอเรชั่น จำกัด (มหาชน) (TRUE) และ บริษัท โทเทิ่ล แอ็คเซ็ส คอมมูนิเคชั่น จำกัด (มหาชน) (DTAC)
-    เป็นบริษัทร่วมทุนระหว่างเครือเจริญโภคภัณฑ์ และเทเลนอร์ เอเชีย เกิดขึ้นจากการควบรวมกิจการกันระหว่าง ทรู คอร์ปอเรชั่น เดิม และดีแทค
-    ในรูปแบบพันธมิตรที่เท่าเทียมกันเพื่อสร้างบริษัทโทรคมนาคมแห่งใหม่ที่สามารถตอบโจทย์ในยุคดิจิทัลได้อย่างเต็มภาคภูมิ รวมถึงมีหลากหลายสาขา
+    เป็นพลังขับเคลื่อนเชื่อมโยงผู้คน กลุ่มธุรกิจ และสังคม ให้เติบโตก้าวหน้าอย่างยั่งยืน ทั้งจากบริการโทรผ่านเสียงและการใช้งานดาต้าระดับเวิลด์คลาสของทรู
+    ระบบนิเวศดิจิทัลที่ตอบโจทย์ทุกไลฟ์สไตล์ของชีวิต ตลอดจนความบันเทิงระดับโลก การมอบสิทธิพิเศษ และการให้บริการเชื่อมต่อแบบไร้รอยต่อ ยิ่งไปกว่านั้น
+    ทรู คอร์ปอเรชั่น ยังมุ่งนำเทคโนโลยีปัญญาประดิษฐ์ (AI) สร้างสรรค์นวัตกรรม ร่วมสร้างคุณค่าให้โลกมีความสมบูรณ์ยั่งยืนและปลอดภัยยิ่งกว่า
+
+    "True" - หมายถึงการพูดถึงทรูในลักษณะกว้าง ๆ โดยไม่ได้ระบุผลิตภัณฑ์หรือบริการเฉพาะเจาะจง
+    เช่น การกล่าวถึงบริษัททรู, แบรนด์ทรู, หรือบริการทรูโดยรวม หากในข้อความมีการพูดถึงคำว่า
+    'ทรู' หรือ 'True' โดยไม่มีคำอื่นที่เกี่ยวข้องกับผลิตภัณฑ์หรือบริการเฉพาะ
+    - **!!หากไม่สามารถ จำแนก product ทรูได้ชัดเจน ให้ตอบ "TRUE"**
+
+    "dtac" - ชื่อเดิม dtac 5G = dtac
+    อินเตอร์เน็ตมือถือ, ให้บริการเชื่อมต่อเครือข่ายที่แข็งแกร่งและรวดเร็วครอบคลุมทั่วไทย
+    ในฐานะผู้นำบริการโทรคมนาคมเพียงรายเดียวที่มีคลื่นความถี่ครบทุก 8 ย่าน ทั้งเครือข่าย 5G และ 4G
+    ด้วยจำนวนเสาสัญญาณที่มีมากกว่าหลายหมื่นแห่งทั่วประเทศ พร้อมให้บริการเครือข่ายครอบคลุม 99% ของประชากรแล้ว
+
+    "์Nan" - ในข้อความไม่ได้เกี่ยวอะไรกับ product TRUE หรือ DTAC หรือ ผลิตภัณฑ์ หรือ ไม่มีการพูดถึง ทรู หรือ ดีแทค เช่น สนใจค่ะ
     """
     output: OutputOptions
+
+class ClassifyTrue(BaseModel):
+    Classify: ClassifyItem
+    Classify_output: Item
 
 
 system_prompt = """
 
 PERSONA:
-You're Social Analysis from TRUE Company, a telecom company with multiple partners.
+You're Social Analysis from TRUE or DTAC Company, a telecom company with multiple partners.
 
 TASKS:
-1. Classify the product of from user quotes.
+1. Classify the product of from user quotes return one-by-one.
 
-INPUT SOURCE:
+INPUT SOURCE
 Many phases gathered from news tablets and Facebook pages.
 
 
@@ -103,47 +128,41 @@ Many phases gathered from news tablets and Facebook pages.
 You are an AI model that classifies user quotes into one or more True Corporation services.
 Follow these classification rules with high accuracy and efficiency:
 
-🔹 **Level 1: ตรวจสอบว่ามี "ทรู" หรือ "true" ในข้อความหรือไม่**
-- **ถ้ามี** → ดำเนินการจำแนกตามหมวดหมู่ด้านล่าง
-- **ถ้าไม่มี** → ให้ classify เป็น `"Nan"` ทันที
+🔹 **Level 1: ตรวจสอบว่ามี "ทรู" หรือ "true" หรือ "ค่ายแดง" หรือ "1242" หรือ "Dtac" หรือ "ดีแทค" หรือ "1678" ในข้อความหรือไม่**
+   - **ถ้ามี** → ดำเนินการจำแนกตามหมวดหมู่ด้านล่าง
+   - **ถ้าไม่มี** → ให้ classify เป็น `"Nan"` ทันที
 
-🔹 **Level 2: ตรวจสอบประเภทผลิตภัณฑ์ (ถ้ามี "ทรู" หรือ "true")**
-1. **True You**: หากพบ **"ร้านเด็ด"**, **"คะแนน"**, **"แต้ม"**, **"จัดแคมเปญ"**.
-2. **True Corp**: หากพบ **"คอร์ปอเรชั่น"**, **"บริษัททรู"**, **"ซีอีโอทรู"**, **"ทรูดีแทค"**, **"ทรู ดิจิทัล กรุ๊ป"**, **"จับมือ"**, **"ทรู-ดีแทค"**,
-**"ทรู ต้อนรับ"**, **"เอส เอฟ จับมือ ทรูดีแทค"**, **"บริษัทฯ"**, **"ลูกค้าทรู ดีแทค"**, **"True  + Dtac"**.
+🔹 **Level 2: ตรวจสอบประเภทผลิตภัณฑ์ (ถ้ามี "ทรู" หรือ "true" หรือ "ค่ายแดง" หรือ "1242")**
+1. **True You**: หากพบ **"ร้านเด็ด"**, **"คะแนน"**, **"แต้ม"**, **"จัดแคมเปญ"**, **"truepoint"**.
+2. **True Corp**: หากพบ **"คอร์ปอเรชั่น"**, **"บริษัท ทรู"**, **"ซีอีโอทรู"**, **"ทรูดีแทค"**, **"ทรู ดิจิทัล กรุ๊ป"**, **"จับมือ"**,
+   **"ทรู ต้อนรับ"**, **"เอส เอฟ จับมือ ทรูดีแทค"**, **"บริษัทฯ"**, **"ลูกค้าทรู ดีแทค"**, **"True + Dtac"**.
 3. **True iService**: หากพบ **"true iservice"**, **"ทรู เซอร์วิส"**, **"แจ้งยอดชำระ"**, **"True Service"**, **"ไอเซอร์วิส"**.
-4. **True Visions NOW**: หากพบ **"TrueVisions Now"**, **"TrueVisions Now Max"**, **"รวมความบันเทิงจากช่องชั้นนำของทรูวิชั่นส์"**.
+4. **True Visions NOW**: หากพบ **"TrueVisions Now"**, **"TrueVisions Now Max"**, **"รวมความบันเทิงจากช่องชั้นนำของทรูวิชั่นส์"**, **"TVS NOWS"**.
 5. **True Visions**: หากพบ **"ทรูวิชั่น"**, **"truevision"**, **"ทรูส์วิชั่น"**, **"TRUE Vision"**.
 6. **True ID**: หากพบ **"True id"**, **"ทรูไอดี"**, **"TrueID"**, **"trueid"**.
 7. **True Online**: หากพบ **"เน็ตบ้าน"**, **"true fiber"**, **"ทรูไฟเบอร์"**, **"ติดตั้งเน็ตบ้าน"**, **"Fiber Stand alone"**, **"โปรย้ายค่ายเน็ตบ้าน"**,
-**"WiFiที่บ้าน"**, **"Fiber"**, **"แพ็กเกจเน็ตบ้านไฟเบอร์True"**, **"กล่องเราท์เตอร์แดง"**, **"แพคเกจเน็ตบ้านทรู"**, **"สนใจติดตั้งเน็ทบ้าน"**, **"True wifi"**.
-8. **True 5G**: หากพบ **"เน็ตโทรศัพท์"**, **"เปิดเน็ตเบอร์โทร"**, **"อินเตอร์เน็ตแบบเติมเงิน"**, **"ทรูมูฟ"**, **"ซิม"**, **"โปรโมชั่น"**,
-**"เครือข่ายดีแทค"**, **"เปิดสัญญาณ"**, **"มือถือเก่ามาเเลกใหม่"**, **"เน็ตห่วย"**, **"แพคเกจ"**, **"ย้ายค่าย"**, **"เน็ททรู"**, **"eSIM"**,
-**"เบอร์รายเดือน"**, **"เน็ตดีแทค"**, **"trueshop"**, **"ทรูช็อป"**, **"เล่นเน็ตไม่อั้น"**, **"คลื่น"**, **"โปรข้ามเวลา"**, **"รายเดือนTruemove"**,
-**"รายเดือนทรู"**, **"เน็ตทรูกับดีแทค"**, **"โทรมาขายโปร"**, **"ทรูแบบเติมเงิน"**, **"ทรูช่วยยกเลิกเน็ต"**, **"เก็บค่าบริการโรมมิ่ง"**,
-**"เน็ตทรูรายเดือนคุ้มๆ"**, **"สมัครโรมมิ่งทรู"**, **"เบอร์ทรู"**.
+   **"WiFiที่บ้าน"**, **"Fiber"**, **"แพ็กเกจเน็ตบ้านไฟเบอร์True"**, **"กล่องเราท์เตอร์แดง"**, **"แพคเกจเน็ตบ้านทรู"**, **"สนใจติดตั้งเน็ทบ้าน"**, **"True wifi"**.
+8. **True 5G**: หากพบ **"เน็ตโทรศัพท์"**, **"เปิดเน็ตเบอร์โทร"**, **"อินเตอร์เน็ตแบบเติมเงิน"**, **"ทรูมูฟ"**, **"ซิม"**, **"โปรโมชั่น"**, **"Truedtac5G"**
+   **"เครือข่ายดีแทค"**, **"เปิดสัญญาณ"**, **"มือถือเก่ามาเเลกใหม่"**, **"แพคเกจ"**, **"ย้ายค่าย"**, **"เน็ททรู"**, **"eSIM"**,
+   **"เบอร์รายเดือน"**, **"เน็ตดีแทค"**, **"trueshop"**, **"ทรูช็อป"**, **"เล่นเน็ตไม่อั้น"**, **"คลื่น"**, **"โปรข้ามเวลา"**, **"รายเดือนTruemove"**,
+   **"รายเดือนทรู"**, **"เน็ตทรูกับดีแทค"**, **"โทรมาขายโปร"**, **"ทรูแบบเติมเงิน"**, **"ทรูช่วยยกเลิกเน็ต"**, **"เก็บค่าบริการโรมมิ่ง"**,
+   **"เน็ตทรูรายเดือนคุ้มๆ"**, **"สมัครโรมมิ่งทรู"**, **"เบอร์ทรู"**.
 9. **TRUE (General Category)**: หากพบ **"ค่ายแดง"**, **"สัญญาณ"**, **"true"**, **"ย้ายค่าย"**, **"ทรู"**, **"ช่องทรู"**, **"กล่องทรู"**,
-**"พนักงานทรู"**, **"เน็ต"**, **"เจ้าหน้าที่ทรู"**, **"ติดต่อทรู"**, **"1242"**, **"เปิดสัญญาณทรู"**, **"กลยุทธ์ของทรู"**, **"สาขาทรู"**
-➜ **รวมถึงกรณีที่พูดถึง "เน็ต" แต่ไม่ได้ระบุว่าเป็น "เน็ตบ้าน" หรือ "เน็ตโทรศัพท์"**
-➜ **ให้ classify เป็น `"TRUE"`**
+   **"พนักงานทรู"**, **"เน็ต"**, **"เจ้าหน้าที่ทรู"**, **"ติดต่อทรู"**, **"1242"**, **"เปิดสัญญาณทรู"**, **"กลยุทธ์ของทรู"**, **"สาขาทรู"**
+   ➜ **รวมถึงกรณีที่พูดถึง "เน็ต" แต่ไม่ได้ระบุว่าเป็น "เน็ตบ้าน" หรือ "เน็ตโทรศัพท์"**
+   ➜ **ให้ classify เป็น `"TRUE"`**
 
-🔹 **Special Case: "Nan"**
-- หาก **ไม่มีคำว่า "ทรู" หรือ "true"** → ให้ classify เป็น `"Nan"` ทันที เช่น **"สอบถามเรื่องเก่าแลกใหม่ครับ เงื่อนไข รายละเอียดเป็นอย่างไรครับ"**
+   **Level 3: ตรวจสอบประเภทผลิตภัณฑ์ (ถ้ามี "Dtac" หรือ "ดีแทค" หรือ "1678")**
+   **Dtac**: หากพบ "Dtac, DTAC, ดีแทค, ค่ายฟ้า, detac, dtac, 1678"
 
 """
 
-
 system_prompt_2 = """
-
-**!!IMPORTANT: RETURN OUTPUT FOR EVERY INPUT PHASE. DO NOT OMIT ANY ITEM!!**
-
 PERSONA:
 You're Social Analysis from TRUE Company, a telecom company with multiple partners.
 
 TO DO: !!MUST ANSWER EVERY PHASE **CANNOT SKIP OR RETURN NONE FOR ANY PHASE**
 ALWAYS RETURN OUTPUT FOR EVERY PHASE, EVEN IF IT'S UNKNOWN.
-
-ANSWER SHOULD BE: Negative, Neutral, Positive.
 
 TASKS:
 1. Find the sentiment of these user quotes and return the output format for all {phase} phases one-by-one.
@@ -157,5 +176,5 @@ BASIC CRITERIA FOR SENTIMENT ANALYSIS:
 - Conversely, if the phase is bad for TRUE Company's reputation, classify it as Negative.
 - If the phase shows interest such as "สนใจ", classify it as Positive.
 
-**!!IMPORTANT: RETURN OUTPUT FOR EVERY INPUT PHASE. DO NOT OMIT ANY ITEM!!**
+!!IMPORTANT: RETURN OUTPUT FOR EVERY INPUT PHASE. DO NOT OMIT ANY ITEM!!
 """
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 from Uploadfile.upload import *
 from openai import OpenAI, AsyncOpenAI
-from Models.model import SentimentItem, TRUEItem, ADS, SentimentTrue, OutputOptions, Item, system_prompt, system_prompt_2
+from Models.model import SentimentItem, SentimentTrue, OutputOptions, Item, system_prompt, system_prompt_2, ClassifyItem, ClassifyTrue
 from io import BytesIO
 import asyncio
 import openai
@@ -29,29 +29,11 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 #     col2 = [row[1] for row in msg]  # ดึงค่า col2 เก็บเป็น list
 #     col3 = [row[2] for row in msg]
 
-
-# เพิ่ม keywords สำหรับแต่ละประเภท
-keywords_dict = {
-    OutputOptions.True_You: ["ร้านเด็ด", "คะแนน", "แต้ม", "จัดแคมเปญ", "ทรูพอยท์"],
-    OutputOptions.True_Corp: ["คอร์ปอเรชั่น", "บริษัททรู", "ซีอีโอทรู", "ทรูดีแทค", "ทรู ดิจิทัล กรุ๊ป", "จับมือ", "ทรู-ดีแทค", "ทรู ต้อนรับ", "เอส เอฟ จับมือ ทรูดีแทค", "บริษัทฯ", "ลูกค้าทรู ดีแทค", "True  + Dtac"],
-    OutputOptions.True_iService: ["true iservice", "ทรู เซอร์วิส", "แจ้งยอดชำระ","True Service", "ไอเซอร์วิส"],
-    OutputOptions.True_Visions_NOW: ["TrueVisions Now", "TrueVisions Now Max", "รวมความบันเทิงจากช่องชั้นนำของทรูวิชั่นส์", "ทรูวิชั่นส์ นาว"],
-    OutputOptions.True_Visions: ["ทรูวิชั่น", " truevision", "ทรูส์วิชั่น", "TRUE Vision"],
-    OutputOptions.True_ID: ["True id", "ทรูไอดี", "TrueID", "trueid"],
-    OutputOptions.True_Online: ["เน็ตบ้าน", "true fiber", "ทรูไฟเบอร์", "ติดตั้งเน็ตบ้าน", "Fiber Stand alone", "โปรย้ายค่ายเน็ตบ้าน", "WiFiที่บ้าน", "Fiber", "แพ็กเกจเน็ตบ้านไฟเบอร์True", "กล่องเราท์เตอร์แดง",
-                                "แพคเกจเน็ตบ้านทรู", "สนใจติดตั้งเน็ทบ้าน", "True wifi"],
-    OutputOptions.True_5G: ["เน็ตโทรศัพท์", "เปิดเน็ตเบอร์โทร", "อินเตอร์เน็ตแบบเติมเงิน", "ทรูมูฟ", "ซิม", "โปรโมชั่น", "เครือข่ายดีแทค", "เปิดสัญญาณ", "เน็ตห่วย",
-                            "แพคเกจ", "เน็ททรู", "eSIM", "เบอร์รายเดือน", "เน็ตดีแทค", "trueshop", "เครื่องเปล่า", "เล่นเน็ตไม่อั้น", "คลื่น", "โปรข้ามเวลา", "รายเดือนTruemove", "รายเดือนทรู",
-                            "เน็ตทรูกับดีแทค", "โทรมาขายโปร", "ทรูแบบเติมเงิน", "ทรูช่วยยกเลิกเน็ต", "เก็บค่าบริการโรมมิ่ง", "เน็ตทรูรายเดือนคุ้มๆ", "สมัครโรมมิ่งทรู", "เบอร์ทรู", "เติมเงินเน็ต", "truemoveH"],
-    OutputOptions.True_: ["ค่ายแดง", "สัญญาณ", "true", "ย้ายค่าย", "ทรู", "ช่องทรู", "กล่องทรู", "พนักงานทรู", "เน็ต", "เจ้าหน้าที่ทรู", "ติดต่อทรู", "1242", "เปิดสัญญาณทรู", "กลยุทธ์ของทรู", "สาขาทรู", "ทรูช็อป"],
-}
-
 sentiment_exp = []
 sentiment_val = []
 IS_TRUE_exp = []
 IS_TRUE_val = []
-ADS_exp = []
-ADS_val = []
+classify_exp = []
 
 classify_val = []
 classify_new = []
@@ -61,7 +43,7 @@ classify_new = []
 
 if openai_api_key:
     openai.api_key = openai_api_key
-    model = "gpt-4o-mini"
+    model = "gpt-4o-mini" #เปลี่ยน model ได้
 
     client = AsyncOpenAI(api_key=openai.api_key)
 
@@ -70,56 +52,35 @@ if openai_api_key:
         qa_list = qa_test.strip().split("\n")
         qa_cleaned = [line.split(". ", 1)[1] for line in qa_list if ". " in line]
         classy_final = []  # ลิสต์สำหรับเก็บผลลัพธ์
+        sentiment_final = []
 
-        def classify_text(text):
-            # คำที่บ่งบอกว่าเป็นของ True
-            true_keywords = ["ทรู", "true", "ค่ายแดง", "dtac", "ดีแทค", "1242"]
-
-            # ถ้าไม่มีคำที่เกี่ยวกับ True เลย → Nan
-            if not any(word in text.lower() for word in true_keywords):
-                return "Nan"
-
-            # ถ้ามีคำที่เกี่ยวกับ True → ใช้ keywords_dict classify
-            for category, keywords in keywords_dict.items():
-                if any(keyword.lower() in text.lower() for keyword in keywords):
-                    return category
-
-            return "Nan"  # ถ้าไม่เข้าหมวดหมู่ไหนเลยให้เป็น Nan
-
-
-        # ใช้ keywords_dict และเงื่อนไข "ทรู" ก่อน
+        # วนลูปผ่านข้อความแต่ละรายการใน qa_cleaned
         for qa in qa_cleaned:
-            classified_output = classify_text(qa)
-
-            if classified_output is None:  # ถ้า keyword หาไม่เจอ ให้ใช้ LLM
-                completion = await client.beta.chat.completions.parse(
-                    model=model,
-                    messages=[
-                        {"role": "system", "content": system_prompt.format(phase=len(qa.split("\n")))},
-                        {"role": "user", "content": qa},
-                    ],
-                    response_format=Item,
-                )
-                classy = completion.choices[0].message.parsed
-
-            else:
-                classy = classified_output  # ใช้ค่าที่ได้จาก keyword classify
-
+            completion = await client.beta.chat.completions.parse(
+                model=model,
+                messages=[
+                    {"role": "system", "content": system_prompt.format(phase=len(qa.split("\n")))},
+                    {"role": "user", "content": qa},
+                ],
+                response_format=ClassifyTrue,
+            )
+            classy = completion.choices[0].message.parsed  # ดึงผลลัพธ์จาก API
             classy_final.append(classy)
 
-        completion = await client.beta.chat.completions.parse(
-            model=model,
-            messages=[
-                {"role": "system", "content": system_prompt_2.format(phase=len(qa_test.split("\n")))},
-                {"role": "user", "content": qa_test},
-            ],
-            response_format=SentimentTrue,
-        )
-        sentiment = completion.choices[0].message.parsed
+            completion = await client.beta.chat.completions.parse(
+                model=model,
+                messages=[
+                    {"role": "system", "content": system_prompt_2.format(phase=len(qa.split("\n")))},
+                    {"role": "user", "content": qa},
+                ],
+                response_format=SentimentTrue,
+            )
+            sentiment = completion.choices[0].message.parsed
+            sentiment_final.append(sentiment)
 
         output = {
-            "Sentiment": sentiment,
-            "Classify": classy_final
+        "Sentiment": sentiment_final,
+        "Classify": classy_final
         }
 
         return output
@@ -153,49 +114,33 @@ if openai_api_key:
         classify_new = []
         # วนลูปผ่านข้อมูลแต่ละ event
         for event in final_result:
-            # ดึงข้อมูลจาก Sentiment
-            sentiment_exp.extend([item.explanation for item in event['Sentiment'].Sentiment])
-            sentiment_val.extend([item.output for item in event['Sentiment'].Sentiment])
-
-            # ดึงข้อมูลจาก IS_TRUE
-            IS_TRUE_exp.extend([item.explanation for item in event['Sentiment'].IS_TRUE])
-            IS_TRUE_val.extend([item.output for item in event['Sentiment'].IS_TRUE])
-
-            # ดึงข้อมูลจาก ADS
-            ADS_exp.extend([item.explanation for item in event['Sentiment'].ADS])
-            ADS_val.extend([item.ADS for item in event['Sentiment'].ADS])
-
-
-            # ดึงข้อมูลจาก Classify (วนลูปในลิสต์)
-            classify_val.extend([
-                cls.output.value if isinstance(cls, Item) else
-                cls.value if isinstance(cls, OutputOptions) else
-                cls
-                for cls in event['Classify']
-            ])
-
-            # classify_val.extend([cls.output.value for cls in event['Classify']])
-            # classify_val.extend([cls.output if isinstance(cls, Item) else cls for cls in event['Classify']])
+            for i in range(len(event['Sentiment'])):
+                sentiment_exp.append(event['Sentiment'][i].Sentiment[0].explanation)
+                sentiment_val.append(event['Sentiment'][i].Sentiment[0].output)
+                IS_TRUE_exp.append(event['Sentiment'][i].IS_TRUE[0].explanation)
+                IS_TRUE_val.append(event['Sentiment'][i].IS_TRUE[0].output)
+            for i in range(len(event['Classify'])):
+                classify_exp.append(event['Classify'][i].Classify.explanation)
+                classify_val.append(event['Classify'][i].Classify_output.output.value)
 
         # แสดงผลข้อมูลในแต่ละ list
         min_length = len(msg)
         sentiment_exp = sentiment_exp[:min_length]
         sentiment_val = sentiment_val[:min_length]
-        # ans = ans[:min_length]
+        # ans = my_classify[:min_length]
         # human_ans = human_ans[:min_length]
         IS_TRUE_exp = IS_TRUE_exp[:min_length]
         IS_TRUE_val = IS_TRUE_val[:min_length]
-        ADS_exp = ADS_exp[:min_length]
-        ADS_val = ADS_val[:min_length]
+        classify_exp = classify_exp[:min_length]
         classify_val = classify_val[:min_length]
 
         st.write(
             len(sentiment_exp), len(sentiment_val), len(IS_TRUE_exp), len(IS_TRUE_val),
-            len(ADS_exp), len(ADS_val), len(classify_val)
+            len(classify_exp), len(classify_val)
         )
 
 
-        if len(sentiment_exp) == len(sentiment_val) == len(IS_TRUE_exp) == len(IS_TRUE_val) == len(ADS_exp) == len(ADS_val) == len(classify_val):
+        if len(sentiment_exp) == len(sentiment_val) == len(IS_TRUE_exp) == len(IS_TRUE_val) == len(classify_exp) == len(classify_val):
             # ใช้ st.columns เพื่อแยกพื้นที่ออกเป็นสองคอลัมน์
             col1, col2 = st.columns(2)
 
@@ -243,11 +188,14 @@ if openai_api_key:
                     "Message": msg,
                     "LLM_Reason": sentiment_exp,
                     "LLM": sentiment_val,
+                    # "Answer": ans,
+                    # "Human_Answer": human_ans,
                     "IS_TRUE_explain": IS_TRUE_exp,
                     "IS_TRUE_value": IS_TRUE_val,
-                    "ADS_explain": ADS_exp,
-                    "ADS_value": ADS_val,
+                    "Classify_explain": classify_exp,
                     "classify_value": classify_val,
+                    # "true_classify": true_classify,
+                    # "my_classify": cleaned_ans
                 })
                 st.write(gpt_4o_mini.head())  # แสดง preview
 
